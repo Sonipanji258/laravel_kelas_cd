d5cf5b950a91f8aa0a3c1fc0e90618dd6d7b746e
sonipanji258 <sonipanji123@gmail.com>
2020-07-14 12:10:46 +0700

    pertemuan 11 : Membuat Halaman Template

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('tittle','Halaman Mahasiswa')
+@section('bread1','Mahasiswa')
+@section('bread2','Daftar')
+@section('content')
+    <p>This is my body content.</p>
+@endsection
